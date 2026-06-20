a52000d0b47cec550a439d513ebe0096a42f0e43
josoyean <dlfjswhtnals@naver.com>
2026-06-20 15:45:20 +0900

    애널리틱스 추가

--- src/api/supabase-api.ts
+++ src/api/supabase-api.ts
@@ -1,10 +1,11 @@
-import { supabaseKey, supabaseUrl } from "../supabase";
 import axios from "axios";
+import { supabaseKey, supabaseUrl } from "../supabase";
 export async function getProjects() {
   try {
     const response = await axios.get(`${supabaseUrl}/rest/v1/oilve-shop`, {
       headers: {
         apikey: supabaseKey,
+        Authorization: `Bearer ${supabaseKey}`,
       },
     });
     console.log("데이터 패칭 성공", response.data);
--- src/supabase.tsx
+++ src/supabase.tsx
@@ -3,9 +3,8 @@ import { Database } from "supabase.types";
 
 const supabase = createClient<Database>(
   `${import.meta.env.VITE_SUPABASE_URL}`,
-  import.meta.env.VITE_SUPABASE_KEY
+  import.meta.env.VITE_SUPABASE_KEY,
 );
 export { supabase };
-
 export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 export const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
